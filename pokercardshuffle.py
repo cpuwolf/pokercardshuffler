8eae302f6e880c895b6fcdd50251db3d88c6a865
Wei Shuai <cpuwolf@gmail.com>
2021-11-09 16:07:12 +0800

    full screen app

--- pokercardshuffle.py
+++ pokercardshuffle.py
@@ -21,5 +21,5 @@ def resource_path(relative_path):  # needed for bundling
     base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
     return os.path.join(base_path, relative_path)
 
-window = webview.create_window('cpuwolf@gmail.com', resource_path('index.html'))
+window = webview.create_window('cpuwolf@gmail.com', resource_path('index.html'), fullscreen=True)
 webview.start()
